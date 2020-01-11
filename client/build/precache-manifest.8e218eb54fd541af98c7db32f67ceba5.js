d52ac5536c2ee8ce3e2fa4d024bb118e26247e93
mkallgren08 <mkallgren08@gmail.com>
2020-01-11 00:05:43 -0500

    Building for production

---
+++ client/build/precache-manifest.8e218eb54fd541af98c7db32f67ceba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b2c71465df78e685fdd2cecaa78a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56dff2ed9d48085f68da",
+    "url": "/static/css/main.4c8bae49.chunk.css"
+  },
+  {
+    "revision": "8e83fb4a574b062dbe69",
+    "url": "/static/js/2.92bccda9.chunk.js"
+  },
+  {
+    "revision": "56dff2ed9d48085f68da",
+    "url": "/static/js/main.bc796099.chunk.js"
+  },
+  {
+    "revision": "19265050327c31fa2fdd",
+    "url": "/static/js/runtime~main.7242f724.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  }
+]);
